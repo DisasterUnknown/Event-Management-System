023809d4bb1e35074c5669acaca37838597dda68
DisasterUnknown <priyashanthafdo26@gmail.com>
2024-08-18 20:22:54 +0530

    Added the View event page

--- Root Folder/Properties/Resources.Designer.cs
+++ Root Folder/Properties/Resources.Designer.cs
@@ -179,5 +179,15 @@ namespace Root_Folder.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap viewEventBg {
+            get {
+                object obj = ResourceManager.GetObject("viewEventBg", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
---
+++ Root Folder/ViewEvent.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace Root_Folder
+{
+    partial class ViewEvent
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            dateTimePicker1 = new DateTimePicker();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Rockwell Extra Bold", 20F, FontStyle.Bold);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(165, 26);
+            label1.Name = "label1";
+            label1.Size = new Size(262, 40);
+            label1.TabIndex = 0;
+            label1.Text = "Event Name";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.BackgroundImage = Properties.Resources.backGlowBtn1;
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox1.Location = new Point(-5, -5);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(63, 60);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(103, 99);
+            label2.Name = "label2";
+            label2.Size = new Size(79, 28);
+            label2.TabIndex = 2;
+            label2.Text = "Name: ";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(103, 148);
+            label3.Name = "label3";
+            label3.Size = new Size(67, 28);
+            label3.TabIndex = 3;
+            label3.Text = "Place:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(103, 197);
+            label4.Name = "label4";
+            label4.Size = new Size(64, 28);
+            label4.TabIndex = 4;
+            label4.Text = "Time:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Transparent;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label5.ForeColor = Color.White;
+            label5.Location = new Point(103, 244);
+            label5.Name = "label5";
+            label5.Size = new Size(64, 28);
+            label5.TabIndex = 5;
+            label5.Text = "Price:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.Transparent;
+            label6.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label6.ForeColor = Color.White;
+            label6.Location = new Point(103, 292);
+            label6.Name = "label6";
+            label6.Size = new Size(201, 28);
+            label6.TabIndex = 6;
+            label6.Text = "Participant amount:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(335, 103);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(165, 27);
+            textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(335, 152);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(165, 27);
+            textBox2.TabIndex = 8;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.CustomFormat = "dd/MM/yyyy h:mm tt";
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            dateTimePicker1.Location = new Point(335, 199);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(165, 27);
+            dateTimePicker1.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(335, 248);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(165, 27);
+            textBox3.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(335, 296);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(165, 27);
+            textBox4.TabIndex = 11;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(50, 349);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(504, 226);
+            dataGridView1.TabIndex = 12;
+            // 
+            // ViewEvent
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.viewEventBg;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(612, 606);
+            Controls.Add(dataGridView1);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Cursor = Cursors.Default;
+            Name = "ViewEvent";
+            Text = "View Event";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private DateTimePicker dateTimePicker1;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private DataGridView dataGridView1;
+    }
+}
